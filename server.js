7d5d65b5050e5ce7891eafa45a68e8720ee6abb5
raffleberry <raffleberry42@gmail.com>
2019-03-14 23:16:32 +0530

    added api routes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import posts from './routes/posts.routes';
+
 let app = express();
+
+app.use('/api', posts);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
