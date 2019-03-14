6739b68c390f429b0afd58987518fe3c017ace77
raffleberry <raffleberry42@gmail.com>
2019-03-15 00:58:07 +0530

    deprecated property removed

--- db/connect.js
+++ db/connect.js
@@ -5,8 +5,7 @@ mongoose.Promise = global.Promise;
 
 const connectToDb = async () => {
     try {
-        await mongoose.connect(config.mongoUrl, { useMongoClient: true });
-        
+        await mongoose.connect(config.mongoUrl);
     }
     catch (err) {
         
