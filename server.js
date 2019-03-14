d7b4c544d2f092488b2679b0f77650dd587185c6
raffleberry <raffleberry42@gmail.com>
2019-03-15 00:56:49 +0530

    db connection established

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import posts from './routes/posts.routes';
+import connectToDb from './db/connect';
+
+connectToDb();
 
 let app = express();
 
-app.use('/api', posts);
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(bodyParser.json());
+
+app.use('/api', posts);
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(3005, () => {
