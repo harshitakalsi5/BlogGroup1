dcaf8ed9817a581d03187ef82a3a5312245f9e9f
raffleberry <raffleberry42@gmail.com>
2019-03-22 19:49:56 +0530

    added backend done

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,75 @@
+var totalPosts;
+var totalPostsPages;
+
+function refreshPosts() {
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:3005/api/posts",
+    dataType: 'json',
+    contentType: 'application/json',
+  }).done(function(data, status) {
+    postsx = data;
+    var postsSection = document.querySelector("#posts-section");
+    postsSection.innerHTML = "";
+    if (localStorage.currentPostPage === undefined) {
+      localStorage.currentPostPage = Number(0);
+    }
+    totalPosts = Number(data.posts.length);
+    //here 4 is the no. of posts viewable per page.
+    totalPostsPages = Math.floor(totalPosts / 4);
+    for (var i = 0; i < totalPosts; i++) {
+      if (Math.floor(i/4) === Number(localStorage.currentPostPage)) {
+        var postPara = document.createElement("p");
+        //title
+        postPara.appendChild(document.createElement("h3"));
+        postPara.getElementsByTagName("h3")[0].innerText = data.posts[i].title;
+
+        // postPara.appendChild(document.createElement("br"))
+
+        //post content
+        postPara.appendChild(document.createElement("h6"))
+        console.log(data.posts[i].content);
+        postPara.getElementsByTagName("h6")[0].innerText = String(data.posts[i].content);
+
+        var details = document.createElement("span");
+        details.innerHTML = "Posted by <i><u>" + data.posts[i].author + "</i></u> on " + data.posts[i].dateAdded.split("T")[0];
+        postPara.appendChild(document.createElement("br"));
+        postPara.appendChild(details);
+        var hr = document.createElement("hr");
+        hr.style.width = "200px";
+        hr.classList.add('w3-opacity');
+        postPara.appendChild(hr);
+        postsSection.appendChild(postPara);
+      }
+    }
+  });
+}
+
+var olderBtn = document.getElementById("increment");
+var newerBtn = document.getElementById("decrement");
+
+olderBtn.addEventListener("click", function() {
+  if (localStorage.currentPostPage === undefined) {
+    localStorage.currentPostPage = Number(0);
+  } else {
+    if (Number(localStorage.currentPostPage) + 1 <= totalPostsPages) {
+      localStorage.currentPostPage = Number(localStorage.currentPostPage) + 1;
+    }
+  }
+  refreshPosts();
+});
+
+newerBtn.addEventListener("click", function() {
+  if (localStorage.currentPostPage === undefined) {
+    localStorage.currentPostPage = Number(0);
+  } else {
+    if (Number(localStorage.currentPostPage) - 1 >= 0) {
+      localStorage.currentPostPage = Number(localStorage.currentPostPage) - 1;
+    }
+  }
+  refreshPosts();
+});
+
 $("#btnx").click(function() {
   $.ajax({
     method: "POST",
@@ -17,5 +89,8 @@ $("#btnx").click(function() {
     $("#post-body").val("");
     $("#title").val("");
     $("#email").val("");
+    refreshPosts();
   });
 });
+
+refreshPosts();
--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 export default {
-    "port": 3005,
-    "mongoUrl": "mongodb://localhost:27017/blog",
+    // "mongoUrl": "mongodb://localhost:27017/blog",
+    "mongoUrl": "mongodb://hello:world@cluster0-shard-00-00-sny2j.gcp.mongodb.net:27017,cluster0-shard-00-01-sny2j.gcp.mongodb.net:27017,cluster0-shard-00-02-sny2j.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
     "bodyLimit": "100kb"
 }
